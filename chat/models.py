b6ebe6b7ef96d695001b489555ee2aa55e74ef12
demo-23home <zeyadslama23@gmail.com>
2024-11-29 17:06:37 +0200

    Adding (Group, Chat) Models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+import uuid
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Group(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    members = models.ManyToManyField(User)
+
+    def add_user(self, user: User) -> None:  # type: ignore
+        if user in self.members:
+            raise ValueError("User already exists in the group!")
+
+        self.members.add(user)
+        self.save()
+        return
+
+    def remove_user(self, user: User) -> None:  # type: ignore
+        if user not in self.members:
+            raise ValueError("User is not a member of the group!")
+        self.members.remove(user)
+        self.save()
+        return
+
+    def __str__(self) -> str:
+        return f"Group {uuid}"
+
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    content = models.TextField()
+    time_stamped = models.DateTimeField(auto_now_add=False)
+
+    def __str__(self) -> str:
+        return f"Message by {self.author} in group {self.group}"
