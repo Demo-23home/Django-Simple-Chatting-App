e536c6deba7928d6c22b2ee27fdc74844d0287a8
demo-23home <zeyadslama23@gmail.com>
2024-11-29 02:18:11 +0200

    Initailzing Project files

--- simple_chat_app/django_chat/django_chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('members.urls')),
-    path('', include('chatapp.urls')),
-    path('admin/', admin.site.urls),
-]
